211c84e1028ff87aefaa5d0b4a00fe5760387246
Shatansh6 <pshatansh@gmail.com>
2025-11-16 16:00:21 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Food-Delivery-App-react",
+  base: "/FoodDeliveryApp-react",
 })
